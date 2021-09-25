b39344dffc7b128288b39f32f16ffa8715fe6895
CoopTRUE <coopdog4545@gmail.com>
2021-09-25 15:34:09 -0700

    play encrypted video working

--- main.py
+++ main.py
@@ -2,20 +2,7 @@ from cv2 import CAP_PROP_FPS, VideoCapture, imshow, waitKey, destroyAllWindows
 from time import sleep
 from os import remove
 from uuid import uuid4
-# cap = VideoCapture('trolol.mp4')
-# assert cap.isOpened()
-
-# wait = int(1/cap.get(CAP_PROP_FPS)*1000)
-
-
-# read, frame = cap.read()
-# while read:
-#     imshow('Frame', frame)
-#     read, frame = cap.read()
-#     if waitKey(wait) & 0xFF == ord('q'):
-#         break
-# cap.release()
-# destroyAllWindows()
+from tempfile import NamedTemporaryFile
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.hashes import SHA512
 from cryptography.hazmat.backends import default_backend
@@ -53,9 +40,32 @@ def encrypt_video(remove_old: bool = True):
     with open(new_name, 'wb') as new_file:
         new_file.write(new_data)
 
+
 def play_encrypted_video():
     name = input('Encrypted video name: ')
+    with open(name, 'rb') as file:
+        encrypt_data = file.read()
 
+    password = input('Video password: ')
+    cipher = get_cipher(password.encode(), SALT)
+
+    decrypted_data = cipher.decrypt(encrypt_data)
+
+    with NamedTemporaryFile() as temp:
+        temp.write(decrypted_data)
+
+        cap = VideoCapture(temp.name)
+        assert cap.isOpened()
+
+        wait = int(1/cap.get(CAP_PROP_FPS)*1000)
+        read, frame = cap.read()
+        while read:
+            imshow('Frame', frame)
+            read, frame = cap.read()
+            if waitKey(wait) & 0xFF == ord('q'):
+                break
+        cap.release()
+        destroyAllWindows()
 
 
 def main():
@@ -68,4 +78,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
