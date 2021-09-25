26ee841fcf5915b33e36171c5283afa83221ccb2
CoopTRUE <coopdog4545@gmail.com>
2021-09-25 13:39:12 -0700

    basic encryption function done

--- main.py
+++ main.py
@@ -13,3 +13,29 @@ while read:
     read, frame = cap.read()
     if waitKey(wait) & 0xFF == ord('q'):
         break
+cap.release()
+destroyAllWindows()
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.hashes import SHA512
+from cryptography.hazmat.backends import default_backend
+from base64 import urlsafe_b64encode
+from cryptography.fernet import Fernet
+SALT = b'ilovesalt'
+def encrypt():
+    n = input('Name of file: ')
+    p = input('PASSWORD: ')
+    kdf = PBKDF2HMAC(
+        algorithm=SHA512(),
+        length=32,
+        salt=SALT,
+        iterations=100000,
+        backend=default_backend()
+    )
+    encrypted_password = urlsafe_b64encode(kdf.derive(p.encode()))
+    cipher_suite = Fernet(encrypted_password)
+    with open(n, 'rb') as f:
+        print(cipher_suite.encrypt(f.read()))
+
+
+if __name__ == '__main__':
+    encrypt()
